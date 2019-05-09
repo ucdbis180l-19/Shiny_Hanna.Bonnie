24a80ea01ecb4b8fa34e3402c89d666f2e3d86aa
Ubuntu <ubuntu@ip-172-31-83-166.ec2.internal>
2019-05-09 22:12:51 +0000

    na

--- Shiny_Hanna.Bonnie/server.R
+++ Shiny_Hanna.Bonnie/server.R
@@ -9,6 +9,10 @@
 
 library(shiny)
 library(ggplot2)
+library(tidyverse)
+devtools::install_github("UCDBIS180L/BIS180LTutorials") 
+learnr::run_tutorial("Gather_And_Spread", package = "BIS180LTutorials") 
+
 
 # Define server logic required to draw a boxplot
 shinyServer(function(input, output) {
@@ -23,16 +27,19 @@ shinyServer(function(input, output) {
   output$boxPlot <- renderPlot({
     
     # set up the plot
-    pl <- ggplot(data = iris,
-                 #Use aes_string below so that input$trait is interpreted
+    pl <- ggplot(data = filter(iris, Species==input$species),
+                 #Use aes_string below so that input$species is interpreted
                  #correctly.  The other variables need to be quoted
-                 aes_string(x="Species",
-                            y=input$trait,
-                            fill="Species"
+                 aes_string(y=gather(key = "Traits",value = "value", Sepal.Length, Sepal.Width, Petal.Length, Petal.Width),
+                            iris
                  )
     )
     
-    # draw the boxplot for the specified trait
+    # draw the boxplot for the specified species
     pl + geom_boxplot()
   })
 })
+
+
+
+
--- Shiny_Hanna.Bonnie/ui.R
+++ Shiny_Hanna.Bonnie/ui.R
@@ -9,6 +9,7 @@
 
 library(shiny)
 
+
 # Define UI for application that draws a histogram
 shinyUI(fluidPage( #create the overall page
   
@@ -17,18 +18,17 @@ shinyUI(fluidPage( #create the overall page
   
   # Some helpful information
   helpText("This application creates a boxplot to show difference between",
-           "iris species.  Please use the radio box below to choose a trait",
+           "iris traits  Please use the radio box below to choose a species",
            "for plotting"),
   
-  # Sidebar with a radio box to input which trait will be plotted
+  # Sidebar with a radio box to input which species will be plotted
   sidebarLayout(
     sidebarPanel(
-      radioButtons("trait", #the input variable that the value will go into
-                   "Choose a trait to display:",
-                   c("Sepal.Length",
-                     "Sepal.Width",
-                     "Petal.Length",
-                     "Petal.Width")
+      radioButtons("species", #the input variable that the value will go into
+                   "Choose a species to display:",
+                   c("setosa",
+                     "versicolor",
+                     "virginica")
       )),
     
     # Show a plot of the generated distribution
