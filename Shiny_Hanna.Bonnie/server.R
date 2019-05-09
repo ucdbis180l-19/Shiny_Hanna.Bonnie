1a9cdf97f9e6090e355964a94486ff85bacc666d
Hiu Ling Mak <hmak@ucdavis.edu>
2019-05-09 22:44:44 +0000

    added

--- Shiny_Hanna.Bonnie/server.R
+++ Shiny_Hanna.Bonnie/server.R
@@ -1,12 +1,8 @@
 library(shiny)
 library(ggplot2)
 library(tidyverse)
-<<<<<<< HEAD
-devtools::install_github("UCDBIS180L/BIS180LTutorials") 
-learnr::run_tutorial("Gather_And_Spread", package = "BIS180LTutorials") 
-=======
->>>>>>> 96c982a11b5f6bdbedc430d66a0b9675c0378a03
 
+ricedata <- read_csv("/home/ubuntu/Assignments/Assignment_4_Mak.Hiu.Ling/RiceSNPData/Rice_44K_genotypes.csv.gz",na=c("NA","00"))
 
 # Define server logic required to draw a boxplot
 shinyServer(function(input, output) {
@@ -21,23 +17,13 @@ shinyServer(function(input, output) {
   output$boxPlot <- renderPlot({
     
     # set up the plot
-<<<<<<< HEAD
-    pl <- ggplot(data = filter(iris, Species==input$species),
-                 #Use aes_string below so that input$species is interpreted
-                 #correctly.  The other variables need to be quoted
-                 aes_string(y=gather(key = "Traits",value = "value", Sepal.Length, Sepal.Width, Petal.Length, Petal.Width),
-                            iris
-                 )
-    )
-=======
     pl <- ggplot(data = gather(iris, key= "key", value = "value", Sepal.Length,Sepal.Width,Petal.Length,Petal.Width) %>%
                                  filter(Species == input$species),
                  #Use aes_string below so that input$trait is interpreted
                  #correctly.  The other variables need to be quoted
                  aes(x=key,y=value))
                  #Sepal.Length,Sepal.Width,Petal.Length,Petal.Width
-     
->>>>>>> 96c982a11b5f6bdbedc430d66a0b9675c0378a03
+
     
     # draw the boxplot for the specified species
     pl + geom_boxplot()
