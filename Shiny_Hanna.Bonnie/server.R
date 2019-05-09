d63d23b928390d0fe4e66d1e9946f9ae0fe0be5a
Ubuntu <ubuntu@ip-172-31-83-166.ec2.internal>
2019-05-09 23:29:32 +0000

    a

--- Shiny_Hanna.Bonnie/server.R
+++ Shiny_Hanna.Bonnie/server.R
@@ -2,7 +2,7 @@ library(shiny)
 library(ggplot2)
 library(tidyverse)
 
-ricedata <- read_csv("Rice_44K_genotypes.csv.gz",na=c("NA","00")) %>%
+ricedata <- read_csv("Rice_44K_genotypes.csv.gz",na=c("NA","00")) %>% 
   select(-`6_17160794_1`)
 colnames(ricedata)[1] <- "ID"
 
@@ -15,17 +15,20 @@ shinyServer(function(input, output) {
   #  1) It is "reactive" and therefore should re-execute automatically
   #     when inputs change
   #  2) Its output type is a plot
-  
+   
   output$boxPlot <- renderPlot({
     
     # set up the plot
 
     pl <- ggplot(data = gather(iris, key= "key", value = "value", Sepal.Length,Sepal.Width,Petal.Length,Petal.Width) %>%
-                                 filter(Species == input$species),
                  #Use aes below so that input$trait is interpreted
                  #correctly.  The other variables need to be quoted
-                 aes(x=key,y=value))
+                 
+                 aes(x=xaxistrait, y=yaxistrait, color=colortrait))
                  #Sepal.Length,Sepal.Width,Petal.Length,Petal.Width
+    
+    
+    
 
     # draw the scatter plot for the specified species
     pl + geom_point()
