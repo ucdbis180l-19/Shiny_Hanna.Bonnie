cdb1a3effe42d7b8e817e7f4ced8983b54daa58a
Hiu Ling Mak <hmak@ucdavis.edu>
2019-05-11 06:05:55 +0000

    i think this is pretty much done?

--- App Files Shiny_Hanna.Bonnie/server.R
+++ App Files Shiny_Hanna.Bonnie/server.R
@@ -6,25 +6,17 @@ ricedata <- read_csv("RiceDiversity.44K.MSU6.Phenotypes.csv",na=c("NA","00"))
 summary(ricedata)
 ricedata <- ricedata[c("Seed length", "Seed width", "Seed volume", "Seed surface area", "Plant height")]
 
-# Define server logic required to draw a boxplot
+# Define server logic required to draw a scatterplot
 shinyServer(function(input, output) {
   
-  # Expression that generates a boxplot. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
-   
-  output$boxPlot <- renderPlot({
-    
-    # set up the plot
+  output$scatterPlot <- renderPlot({
 
-    ggplot(data = ricedata, aes_string(x=input$xaxistrait, y=input$yaxistrait, color=input$colortrait,size=I(input$slider)))+
+    ggplot(data = ricedata, aes_string(x=input$xaxistrait, y=input$yaxistrait, color=input$colortrait, size=I(input$slider)))+
+      #make a scatterplot
       geom_point() +
-      geom_text(aes(x=1,y=1), label="test")+
+      #if user checks the box, then display the linear regression line
       if(input$checkbox==TRUE){geom_smooth(method = lm)}
-  })
+  }) 
 })
 
 
--- App Files Shiny_Hanna.Bonnie/ui.R
+++ App Files Shiny_Hanna.Bonnie/ui.R
@@ -1,20 +1,18 @@
-
 library(shiny)
 
-
-
-# Define UI for application that draws a histogram
+# Define UI for application that draws a scatterplot
 shinyUI(fluidPage( #create the overall page
   
   # Application title
-  titlePanel("Iris Data"),
+  titlePanel("Relationship between the phenotypes of 413 rice species"),
   
   # Some helpful information
-  helpText("This application creates a boxplot to show difference between",
-           "iris traits  Please use the radio box below to choose a species",
-           "for plotting"),
+  helpText("This application creates a scatterplot to show the relationship between",
+           "quantitative phenotypes of 413 rice species. Please use the drop down menus below to choose",
+           "three different traits for plotting. Use the slider to change the size of the data points and",
+            "the check box to display the linear regression line."),
   
-  # Sidebar with a radio box to input which species will be plotted
+  # Sidebar with a select boxes to input which traits will be plotted, slide bar for size of the data points and check box for linear regression line.
   sidebarLayout(
     sidebarPanel(
       selectInput("xaxistrait", 
@@ -26,6 +24,8 @@ shinyUI(fluidPage( #create the overall page
       selectInput("colortrait", 
               "Choose a third trait to display:",
               c("`Seed length`","`Seed width`","`Seed volume`", "`Seed surface area`", "`Plant height`")),
-      sliderInput("slider", "Size of dots", min = 1, max = 3, value = 1),
+      
+      sliderInput("slider", "Size of the data points", min = 1, max = 3, value = 1),
+      
       checkboxInput("checkbox", "Display linear regression line", value=FALSE)),
-    mainPanel(plotOutput("boxPlot")))))
+    mainPanel(plotOutput("scatterPlot")))))
